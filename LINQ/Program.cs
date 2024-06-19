28ef73c9a56d7c0c27ca78e02d4b748c65aa53d0
“Noah <urbannoah@yahoo.com>
2024-06-19 10:50:38 -0400

    Linq Exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace LINQ
+﻿using System;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> gameList = new List<string>() { "Overwatch", "Rocket League", "Civ 6", "Driving Sim", "Guitar Hero"};
+
+            IEnumerable<string> ascendingGameList =
+                gameList.OrderBy(name => name.Length);
+
+            foreach (var i in ascendingGameList)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
